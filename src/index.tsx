c6ff735a820ee2fcf9c256da7eea060e78fd473b
Talman95 <taltro528@gmail.com>
2022-10-30 14:56:11 +0300

    updated dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import {App} from "./app/App";
@@ -7,12 +7,16 @@ import {store} from "./store/store";
 import {Provider} from "react-redux";
 import {BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+root.render(
     <Provider store={store}>
         <BrowserRouter>
             <App/>
         </BrowserRouter>
-    </Provider>, document.getElementById('root'));
+    </Provider>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
